436d352684e4c4be4b3911028097370abc464aaa
DanielAngger <angger.7d@gmail.com>
2025-05-06 13:59:28 +0700

    [REFACTOR] Ignoring App.js and App.test.js for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MenuPage from './rating/pages/MenuPage';
-import ProductDetailPage from './rating/pages/ProductDetailPage';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<MenuPage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
